7568e904b96903ad0e4b58a3382c6a674ac01b7d
jtayped <jtayped@gmail.com>
2025-11-23 03:11:34 +0100

    added settings to user dropdown

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -3,7 +3,7 @@ import CurrentUser from "./user";
 
 const Header = () => {
   return (
-    <div className="flex justify-between gap-4">
+    <div className="flex justify-between gap-4 py-6">
       <div /> {/* Display breadcrumbs or so */}
       <CurrentUser />
     </div>
--- src/components/common/header/user.tsx
+++ src/components/common/header/user.tsx
@@ -1,30 +1,71 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuPortal,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { User } from "lucide-react";
+import { Cog, Key, LogOut, PcCase, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
 
 const CurrentUser = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button size={"icon-lg"} variant={"secondary"}>
-          <User />
+        <Button
+          size={"icon-lg"}
+          variant={"secondary"}
+          className="size-12 rounded-full"
+        >
+          <User className="size-5" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <Cog className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+          </DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              {/* Credentials Link */}
+              <DropdownMenuItem asChild>
+                <Link href="/settings/keys" className="w-full cursor-pointer">
+                  <Key className="mr-2 h-4 w-4" />
+                  <span>Credentials</span>
+                </Link>
+              </DropdownMenuItem>
+
+              {/* Nodes Link */}
+              <DropdownMenuItem asChild>
+                <Link
+                  href="/settings/devices"
+                  className="w-full cursor-pointer"
+                >
+                  <PcCase className="mr-2 h-4 w-4" />
+                  <span>Devices</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => signOut()} className="cursor-pointer">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sign out</span>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
